b844f7adf833f854141484d9533aff2379fee81f
Sam Hulick <samh@reelcrafter.com>
2021-12-15 11:52:06 -0600

    Attempting to set up SSR

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,9 +1,27 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { createPageRenderer } from "vite-plugin-ssr";
+
+const renderPage = createPageRenderer({
+  isProduction: true,
+  root: `${__dirname}/../../../../vue`,
+});
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  let url = event.rawPath;
+
+  if (event.rawQueryString) {
+    url += "?" + event.rawQueryString;
+  }
+
+  const pageContextInit = { url };
+  const pageContext = await renderPage(pageContextInit);
+  const { httpResponse } = pageContext;
+  if (!httpResponse) return { statusCode: 200, body: "??" };
+  const { body, statusCode, contentType } = httpResponse;
+
   return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, World! Your request was received at ${event.requestContext.time}.`,
+    statusCode,
+    headers: { "Content-Type": contentType },
+    body,
   };
 };
